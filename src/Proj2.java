bd2964abbd316a5b1e3fc568333e50c5f1bb1596
MassieFlippin <flipwm21@wfu.edu>
2024-10-15 12:52:01 -0400

    AVL Tree working. TestAvl is not working.

--- src/Proj2.java
+++ src/Proj2.java
@@ -1,9 +1,9 @@
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Proj2 {
     public static void main(String[] args) throws IOException {
@@ -27,7 +27,49 @@ public class Proj2 {
         // ignore first line
         inputFileNameScanner.nextLine();
 
-	// FINISH ME
+        // FINISH ME
 
+        //create two array list to store the Real Estate Data
+        ArrayList<RealEstateData> storedArray = new ArrayList<RealEstateData>();
+        //Read the file line and store the data line by line.
+        while (inputFileNameScanner.hasNextLine()) {
+            String line = inputFileNameScanner.nextLine().trim();//trims away any data
+            if (line.isEmpty()) continue; // skip the empty lines
+            String[] parts = line.split(",");
+            //create a new RealEstateData object
+            RealEstateData data = new RealEstateData(
+                    //ID
+                    Integer.parseInt(parts[0]),
+                    //possessionStatus
+                    parts[1],
+                    //Commercial
+                    parts[2],
+                    //Developer
+                    parts[3],
+                    //price
+                    Integer.parseInt(parts[4]),
+                    Integer.parseInt(parts[5]), //sqftprice
+                    parts[6],//furnished
+                    Integer.parseInt(parts[7]), //bathroom
+                    parts[8], //direction facing
+                    parts[9], //transaction
+                    parts[10], //type
+                    parts[11], //city
+                    Integer.parseInt(parts[12]), // bedrooms
+                    Integer.parseInt(parts[13]), //floors
+                    parts[14], //isprimelocation
+                    parts[15]); // lifespan
+
+                    storedArray.add(data);
+        }
+        inputFileNameScanner.close();
+
+        //Sort the Data
+        Collections.sort(storedArray);
+
+        //print the data
+        for(RealEstateData data : storedArray) {
+            System.out.println(data);
+        }
     }
 }
